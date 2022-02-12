eb6b30e1a97a07c6f2d6232b0b43f02daa756be5
Maick Souza <maick_a_s@msn.com>
2022-02-12 14:48:26 -0300

    style(mobile): Home #2 Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,8 +2,30 @@ import { StatusBar } from "react-native";
 import { Container, Header, TotalCars } from "./styles";
 import Logo from "../../assets/logo.svg";
 import { RFValue } from "react-native-responsive-fontsize";
+import { Car } from "../../components/Car";
 
 export function Home() {
+  const carData = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "ao dia",
+      price: 120,
+    },
+    thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png",
+  };
+
+  const carDataTwo = {
+    brand: "Porche",
+    name: "Panamera",
+    rent: {
+      period: "ao dia",
+      price: 120,
+    },
+    thumbnail:
+      "https://www.pngkit.com/png/full/237-2375888_porche-panamera-s.png",
+  };
+
   return (
     <Container>
       <StatusBar
@@ -15,6 +37,9 @@ export function Home() {
         <Logo width={RFValue(108)} height={RFValue(12)} />
         <TotalCars>Total de 12 carros</TotalCars>
       </Header>
+
+      <Car data={carData} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
