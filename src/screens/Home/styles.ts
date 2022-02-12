6aa4cfc259dc65ed05dfcf4d4b7f5ae0d9b747ee
Maick Souza <maick_a_s@msn.com>
2022-02-12 11:50:33 -0300

    chore(mobile): global theme

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,6 +4,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.primary.background};
 `;
 
-export const Text = styled.Text``;
+export const Text = styled.Text`
+  font-size: 30px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+export default {
+  colors: {
+    header: "#1b1b1f",
+
+    primary: {
+      background: "#f4f5f6",
+    },
+    secondary: {
+      background: "#fff",
+    },
+
+    text: {
+      main: "#7a7a80",
+      detail: "#aeaeb3",
+    },
+
+    title: "#47474d",
+
+    main: {
+      main: "#dc1637",
+      light: "#fdedef",
+    },
+
+    success: "#03b252",
+
+    shape: {
+      main: "#e1e1e8",
+      dark: "#29292e",
+    },
+  },
+
+  fonts: {
+    primary_400: "Inter_400Regular",
+    primary_500: "Inter_500Medium",
+
+    secondary_400: "Archivo_400Regular",
+    secondary_500: "Archivo_500Medium",
+    secondary_600: "Archivo_600SemiBold",
+  },
+};
