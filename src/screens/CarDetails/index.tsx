2953bdd57c3c37632148b6f8cea3068d0193052b
Maick Souza <maick_a_s@msn.com>
2022-02-15 16:24:18 -0300

    style(mobile): CarDetails #3

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,19 @@
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
-import { Container, Header, CarImages } from "./styles";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
 export function CarDetails() {
   return (
@@ -16,6 +29,26 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é um auto desportivo Surgiu do lendário touro de lide indultado
+          na praça real Maestranza de Sevilla. É uma belíssimo carro para quem
+          gosta de aceleração.
+        </About>
+      </Content>
     </Container>
   );
 }
