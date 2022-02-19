6da2baa0b00a5cce12c5750e888672e8f94c3199
Maick Souza <maick_a_s@msn.com>
2022-02-19 11:01:21 -0300

    feat(mobile): UserAppointments List

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import { ICarDTO } from "../../dtos/CarDTO";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function MyCars() {
+  const [cars, setCars] = useState<ICarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchCars() {
+    try {
+      const response = await api.get("/schedules_byuser?user_id=1");
+      console.log(response.data);
+      setCars(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCars();
+  }, []);
+
   return <Container></Container>;
 }
