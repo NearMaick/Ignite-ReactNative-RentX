3ef35fb4bd6365930b83dcd9d1d318de9a430b16
Maick Souza <maick_a_s@msn.com>
2022-02-19 16:18:15 -0300

    feat(mobile): SplashScreen #1

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -2,20 +2,31 @@ import { Container } from "./styles";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from "react-native-reanimated";
-import { Button, StyleSheet } from "react-native";
+import { Button, Dimensions, StyleSheet } from "react-native";
+
+const WIDTH = Dimensions.get("window").width;
 
 export function Splash() {
   const animation = useSharedValue(0);
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }],
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bezier(0.53, 0.2, 0, 1),
+          }),
+        },
+      ],
     };
   });
 
   function handleAnimationPosition() {
-    animation.value = Math.random() * 400;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
   return (
