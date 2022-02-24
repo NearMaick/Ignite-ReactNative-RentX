3d96aead45eaba221024ebef7f846d954136d376
Maick Souza <maick_a_s@msn.com>
2022-02-24 10:26:34 -0300

    style(mobile): SignUp #2 First step

--- src/screens/SignUp/FirstStep/index.tsx
+++ src/screens/SignUp/FirstStep/index.tsx
@@ -1,6 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
+import { Keyboard, KeyboardAvoidingView } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
+import { Button } from "../../../components/Button";
+import { Input } from "../../../components/Input";
 import {
   Container,
   Form,
@@ -19,21 +23,30 @@ export function SignUpFirstStep() {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
 
-      <Title>Crie sua {"\n"}conta</Title>
-      <Subtitle>Faça seu cadastro de{"\n"}forma rápida e fácil</Subtitle>
+          <Title>Crie sua {"\n"}conta</Title>
+          <Subtitle>Faça seu cadastro de{"\n"}forma rápida e fácil</Subtitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input iconName="user" placeholder="Nome" />
+            <Input iconName="mail" placeholder="E-mail" />
+            <Input iconName="credit-card" placeholder="CNH" />
+          </Form>
+
+          <Button title="Próximo" />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
