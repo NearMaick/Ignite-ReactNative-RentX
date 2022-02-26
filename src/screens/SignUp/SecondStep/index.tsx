48f07c26c058f2c08b6f8fe92074a98540b23de1
Maick Souza <maick_a_s@msn.com>
2022-02-26 10:51:09 -0300

    feat(mobile): CreateUser

--- src/screens/SignUp/SecondStep/index.tsx
+++ src/screens/SignUp/SecondStep/index.tsx
@@ -7,6 +7,7 @@ import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
 import { Button } from "../../../components/Button";
 import { PasswordInput } from "../../../components/PasswordInput";
+import { api } from "../../../services/api";
 import {
   Container,
   Form,
@@ -40,7 +41,7 @@ export function SignUpSecondStep() {
     goBack();
   }
 
-  function handleRegister() {
+  async function handleRegister() {
     if (!password || !passwordConfirm) {
       return Alert.alert("Informe a senha e a confirmação");
     }
@@ -49,11 +50,24 @@ export function SignUpSecondStep() {
       return Alert.alert("As senhas não são iguais");
     }
 
-    navigate("Confirmation", {
-      nextScreenRoute: "SignIn",
-      title: "Conta criada",
-      message: `Agora é só fazer login\ne aproveitar.`,
-    });
+    await api
+      .post("/users", {
+        name: user.name,
+        email: user.email,
+        driver_license: user.driverLicense,
+        password,
+      })
+      .then(() => {
+        navigate("Confirmation", {
+          nextScreenRoute: "SignIn",
+          title: "Conta criada",
+          message: `Agora é só fazer login\ne aproveitar.`,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert("Opa!", "Não foi possível cadastrar...");
+      });
   }
 
   return (
