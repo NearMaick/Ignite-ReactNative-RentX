dae794d965e5bc110d823a7936b64fd48910fde7
Maick Souza <maick_a_s@msn.com>
2022-03-09 13:48:34 -0300

    fix(web): syncing local database #2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { LoadAnimation } from "../components/LoadAnimation";
 import { useAuth } from "../hooks/auth";
 import { AppTabRoutes } from "./app.tab.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        {user.id ? <AppTabRoutes /> : <AuthRoutes />}
-      </NavigationContainer>
+      {loading ? (
+        <LoadAnimation />
+      ) : (
+        <NavigationContainer>
+          {user.id ? <AppTabRoutes /> : <AuthRoutes />}
+        </NavigationContainer>
+      )}
     </GestureHandlerRootView>
   );
 }
